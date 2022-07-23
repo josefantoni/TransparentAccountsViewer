94782ad7813b530114610aa3a31a64875aaf259e
Josef Antoni <>
2022-07-23 09:28:31 +0200

    Innitial commit, projet creation

---
+++ TransparentAccountsViewer/TransparentAccountsViewer/TransparentAccountsViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentAccountsViewerApp.swift
+//  TransparentAccountsViewer
+//
+//  Created by Pepca on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TransparentAccountsViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
